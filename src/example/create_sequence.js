fe00a8a772005fb521c8e6b7efba49a45843d923
vinccenttt <vincentgoering@gmail.com>
2023-01-28 15:49:59 +0100

    feat: added files + restructure

---
+++ src/example/create_sequence.js
@@ -0,0 +1,246 @@
+import {
+  createOnce,
+  endTracking,
+  hide,
+  TransitionsManager,
+} from "../helper_functions.js";
+
+let svg, viewBox, paddedViewBox, dataset, xScale, yScale;
+
+// setup to use library
+const numberOfSteps = 5;
+const manager = new TransitionsManager(numberOfSteps);
+const drawFunctions = [drawStep0, drawStep1, drawStep2, drawStep3, drawStep4];
+
+setUpvariables();
+setUpNavigation();
+manager.drawStepWithAutoReverse(false, drawFunctions[0]);
+
+function setUpNavigation() {
+  document.getElementById("step").textContent = "Step: 0";
+
+  d3.selectAll(".nav-button").on("click", function () {
+    const id = d3.select(this).attr("id");
+
+    if (manager.getNumberOfActiveTransitions() === 0) {
+      if (id === "next" && manager.getStep() < numberOfSteps - 1) {
+        manager.incrementStep();
+        manager.drawStepWithAutoReverse(
+          false,
+          drawFunctions[manager.getStep()]
+        );
+      } else if (id === "prev" && manager.getStep() > 0) {
+        manager.decrementStep();
+        manager.drawStepWithAutoReverse(true, drawFunctions[manager.getStep()]);
+      }
+
+      document.getElementById("step").textContent =
+        "Step: " + manager.getStep();
+    }
+  });
+}
+
+function setUpvariables() {
+  // determine viewBox measurements
+  viewBox = {
+    width: 1000,
+    height: 500,
+    paddingTop: 150,
+    paddingBottom: 40,
+    paddingLeft: 140,
+    paddingRight: 160,
+  };
+
+  paddedViewBox = {
+    width: viewBox.width - viewBox.paddingLeft - viewBox.paddingRight,
+    height: viewBox.height - viewBox.paddingTop - viewBox.paddingBottom,
+  };
+
+  // create viewBox
+  svg = d3
+    .select("#visualization")
+    .append("svg")
+    .attr("viewBox", "0 0 " + viewBox.width + " " + viewBox.height);
+
+  // create data
+  dataset = [];
+  for (var i = 0; i < 15; i++) {
+    dataset.push(Math.floor(Math.random() * 30) + 3);
+  }
+
+  // create scales
+  xScale = d3
+    .scaleBand()
+    .domain(dataset.map((d, i) => i))
+    .range([viewBox.paddingLeft, viewBox.width - viewBox.paddingRight])
+    .round(true)
+    .paddingInner(0.1);
+
+  yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([paddedViewBox.height + viewBox.paddingTop, viewBox.paddingTop]);
+}
+
+function drawStep0() {
+  //create x-axis
+  const xAxisGenerator = d3.axisBottom().scale(xScale);
+  const xAxis = svg
+    .append("g")
+    .attr("id", "x-axis")
+    .attr("opacity", 1)
+    .attr(
+      "transform",
+      "translate(0," + (paddedViewBox.height + viewBox.paddingTop) + ")"
+    )
+    .call(xAxisGenerator);
+  xAxis.selectAll(".tick text").attr("font-size", "20").attr("y", 15);
+
+
+  // create bars
+  const bars = svg
+    .append("g")
+    .attr("id", "bars")
+    .selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("id", (d, i) => "bar-" + i)
+    .attr("fill", "#4242eb")
+    .attr("width", xScale.bandwidth())
+    .attr("x", (d, i) => xScale(i))
+    .attr("height", 0)
+    .attr("y", viewBox.paddingTop + paddedViewBox.height)
+    .save(manager);
+
+  // transition bars
+  bars
+    .trackedTransition(manager)
+    .delay((d, i) => i * 50)
+    .on("end", function () {
+      endTracking(manager, this);
+    })
+    .attr("y", (d) => yScale(d))
+    .attr("height", (d) => yScale(0) - yScale(d));
+}
+function drawStep1() {
+  // create y-axis
+  const yAxisGenerator = d3.axisLeft().scale(yScale);
+  createOnce("y-axis", () =>
+    svg
+      .append("g")
+      .attr("id", "y-axis")
+      .attr("opacity", 0)
+      .attr("transform", "translate(-10,0)")
+      .save(manager)
+      .call(yAxisGenerator)
+      .selectAll(".tick text")
+      .attr("font-size", "20")
+      .attr("x", -15)
+  );
+
+  svg
+    .select("#y-axis")
+    .trackedTransition(manager)
+    .on("end", function () {
+      endTracking(manager, this);
+    })
+    .attr("transform", "translate(" + viewBox.paddingLeft + ",0)")
+    .attr("opacity", 1);
+}
+function drawStep2() {
+  // create mean line
+  createOnce("mean-vis", () => {
+    const meanVis = svg
+      .append("g")
+      .attr("id", "mean-vis")
+      .attr("opacity", 0)
+      .attr("transform", "translate(0,0)") // needs to be set!
+      .save(manager);
+
+    meanVis
+      .append("rect")
+      .attr("width", paddedViewBox.width + 20)
+      .attr("x", viewBox.paddingLeft)
+      .attr("height", 2);
+
+    meanVis
+      .append("text")
+      .text("mean")
+      .attr("font-size", "20")
+      .attr("font-family", "sans-serif")
+      .attr("text-anchor", "center")
+      .attr("dominant-baseline", "central")
+      .attr("x", viewBox.paddingLeft + paddedViewBox.width + 30);
+  });
+
+  // transition mean line
+  d3.select("#mean-vis")
+    .trackedTransition(manager)
+    .on("end", function () {
+      endTracking(manager, this);
+    })
+    .attr("transform", "translate(0," + yScale(d3.mean(dataset)) + ")")
+    .attr("opacity", 1);
+}
+function drawStep3() {
+  // transiton all bars that are smaller than the mean
+  const bars = svg.selectAll("#bars rect").data(dataset);
+  bars
+    .filter((d) => d < d3.mean(dataset))
+    .trackedTransition(manager)
+    .on("end", function () {
+      endTracking(manager, this);
+    })
+    .attr("fill", "#699dff");
+}
+function drawStep4() {
+  const topThreeValues = [...dataset]
+    .sort((a, b) => (a < b ? 1 : a > b ? -1 : 0))
+    .slice(0, 3);
+
+  // remove mean line
+  svg
+    .select("#mean-vis")
+    .trackedTransition(manager)
+    .on("end", function () {
+      endTracking(manager, this);
+      hide(this);
+    })
+    .attr("transform", "translate(0,0)")
+    .attr("opacity", 0);
+
+  // create text "top values"
+  createOnce("top-values-text", () =>
+    svg
+      .append("text")
+      .attr("id", "top-values-text")
+      .attr("opacity", 0)
+      .text("top values")
+      .attr("font-size", 20)
+      .attr("text-anchor", "middle")
+      .attr("fill", "#ff0000")
+      .attr("font-family", "sans-serif")
+      .attr("y", 0)
+      .attr("x", viewBox.paddingLeft + 0.5 * paddedViewBox.width)
+      .save(manager)
+  );
+
+  // transition text
+  d3.select("#top-values-text")
+    .trackedTransition(manager)
+    .on("end", function () {
+      endTracking(manager, this);
+    })
+    .attr("y", yScale(d3.max(dataset)) - 40)
+    .attr("opacity", 1);
+
+  //transition bars with high values
+  const bars = d3.selectAll("#bars rect").data(dataset);
+  bars
+    .trackedTransition(manager)
+    .attr("fill", (d) => (topThreeValues.includes(d) ? "#ff0000" : "#4242eb"))
+    .on("end", function () {
+      endTracking(manager, this);
+    });
+}
---
+++ src/helper_functions.js
@@ -0,0 +1,133 @@
+d3.selection.prototype.save = function (manager) {
+  this.each(function () {
+    manager.save(d3.select(this));
+  });
+  return this;
+};
+
+d3.selection.prototype.trackedTransition = function (manager) {
+  return this.transition().each(() =>
+    manager.incrementNumberOfActiveTransitions()
+  );
+};
+
+export function endTracking(manager, selection) {
+  manager.save(d3.select(selection));
+  manager.decrementNumberOfActiveTransitions();
+}
+
+export function hide(selection) {
+  d3.select(selection).attr("display", "none");
+}
+
+export function createOnce(id, customFunction) {
+  if (!document.getElementById(id)) {
+    customFunction();
+  } else {
+    const d3Id = "#" + id;
+    d3.select(d3Id).attr("display", "block");
+  }
+}
+
+export class TransitionsManager {
+  #step = 0;
+  #numberOfActiveTransitions = 0;
+  #attributeValuesPerElement = new Map();
+  #changedElementsPerStep = [];
+
+  constructor(numberOfSteps) {
+    for (let i = 0; i < numberOfSteps; i++)
+      this.#changedElementsPerStep[i] = [];
+  }
+
+  revertStep() {
+    let idList = this.#changedElementsPerStep[this.#step + 1];
+    idList.map((id) => this.#resetToPrevState(d3.select("#" + id)));
+    this.#changedElementsPerStep[this.#step + 1] = [];
+  }
+
+  drawStepWithAutoReverse(back, customFunction) {
+    if (back) this.revertStep();
+    else customFunction();
+  }
+
+  getStep() {
+    return this.#step;
+  }
+  incrementStep() {
+    ++this.#step;
+  }
+  decrementStep() {
+    --this.#step;
+  }
+
+  getNumberOfActiveTransitions() {
+    return this.#numberOfActiveTransitions;
+  }
+  incrementNumberOfActiveTransitions() {
+    ++this.#numberOfActiveTransitions;
+  }
+  decrementNumberOfActiveTransitions() {
+    --this.#numberOfActiveTransitions;
+  }
+
+  /* 
+  pushes list of new attributes to object in array attributeValuesPerElement
+  if not existing creates a new entry
+  last entry in attrs array are the newest attributes
+*/
+  save(selection) {
+    const id = selection.attr("id");
+    let element = document.getElementById(id);
+
+    let list = [];
+    for (let i = 0; i < element.attributes.length; i++) {
+      if (element.attributes[i].nodeName !== "id")
+        list.push([
+          element.attributes[i].nodeName,
+          element.attributes[i].nodeValue,
+        ]);
+    }
+
+    const attrsList = this.#attributeValuesPerElement.get(id);
+    if (!attrsList) this.#attributeValuesPerElement.set(id, [list]);
+    else attrsList.push(list);
+
+    this.addIdToStep(id);
+  }
+
+  addIdToStep(id) {
+    const idList = this.#changedElementsPerStep[this.#step];
+    if (!idList.includes(id)) {
+      idList.push(id);
+    }
+  }
+
+  /*
+  resets selection to previous state
+  if only one previous state is left, display ist set to none
+*/
+  #resetToPrevState(selection) {
+    const id = selection.attr("id");
+
+    if (selection.attr("display") === "none")
+      selection.attr("display", "block");
+
+    const attrs = this.#attributeValuesPerElement.get(id);
+    const prevAttrs = attrs[attrs.length - 2];
+
+    const selectionWithTransition = selection
+      .trackedTransition(this)
+      .on("end", () => {
+        attrs.pop();
+        if (attrs.length === 1) {
+          selection.attr("display", "none");
+        }
+        this.decrementNumberOfActiveTransitions();
+      });
+
+    for (let i = 0; i < prevAttrs.length; i++) {
+      selectionWithTransition.attr(prevAttrs[i][0], prevAttrs[i][1]);
+    }
+  }
+}
